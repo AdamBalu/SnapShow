3c6acd840deceeeff2925a4a979058881a1de149
Vladimír Bouček <492927@mail.muni.cz>
2024-04-28 17:44:56 +0200

    fix: signed user dropdown on safari

--- src/components/user-control.tsx
+++ src/components/user-control.tsx
@@ -9,8 +9,8 @@ export const UserControl = async () => {
 	const session = await auth();
 
 	return (
-		<div className="dropdown dropdown-end">
-			<button tabIndex={0} className="btn btn-ghost btn-circle avatar">
+		<details className="dropdown dropdown-end">
+			<summary role="button" className="btn btn-ghost btn-circle avatar">
 				<div className="w-10 rounded-full">
 					<Image
 						width={64}
@@ -19,7 +19,7 @@ export const UserControl = async () => {
 						src={session?.user?.image ?? '/static/user_placeholder.svg'}
 					/>
 				</div>
-			</button>
+			</summary>
 			<ul className="menu menu-sm dropdown-content bg-zinc-900 mt-3 z-[1] p-2 shadow rounded-box w-52">
 				<form className="flex flex-col" action={signOutAction}>
 					<button className="btn btn-ghost uppercase font-extrabold text-xl">
@@ -28,6 +28,6 @@ export const UserControl = async () => {
 				</form>
 				<NavLink href="/profile" label="Profile" />
 			</ul>
-		</div>
+		</details>
 	);
 };
