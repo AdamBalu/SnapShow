6f3f18d8e01d4039eface12595650e02afec4393
Vladimír Bouček <492927@mail.muni.cz>
2024-04-28 15:28:51 +0200

    fix: allow remote images

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,13 @@
-/** @type {import('next').NextConfig} */
+/** @type {import("next").NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'avatars.githubusercontent.com',
-        port: '',
-        pathname: '/u/**',
-      },
-    ],
-  },
+        protocol: "https",
+        hostname: "**"
+      }
+    ]
+  }
 };
 
 export default nextConfig;
