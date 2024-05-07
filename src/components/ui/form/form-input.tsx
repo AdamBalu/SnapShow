999c7114269f4009b198f701cbf7c88069d614cc
Vladimír Bouček <492927@mail.muni.cz>
2024-05-07 21:24:53 +0200

    fix: form input width

--- src/components/ui/form/form-input.tsx
+++ src/components/ui/form/form-input.tsx
@@ -1,55 +1,53 @@
-'use client';
+"use client";
 
-import React, { type HTMLProps } from 'react';
-import { useFormContext } from 'react-hook-form';
+import React, { type HTMLProps } from "react";
+import { useFormContext } from "react-hook-form";
 
-import { cn } from '@/lib/cn';
+import { cn } from "@/lib/cn";
 
 type FormInputProps = HTMLProps<HTMLInputElement> & {
-	label?: string;
-	name: string;
+  label?: string;
+  name: string;
 };
 
 export const FormInput = ({
-	label,
-	name,
-	className,
-	...inputProps
-}: FormInputProps) => {
-	const {
-		register,
-		formState: { errors }
-	} = useFormContext();
+                            label,
+                            name,
+                            className,
+                            ...inputProps
+                          }: FormInputProps) => {
+  const {
+    register,
+    formState: { errors }
+  } = useFormContext();
 
-	return (
-		<div className="form-control w-full">
-			{label && (
-				<label className="label htmlFor={name}">
+  return (
+    <div className="form-control w-full">
+      {label && (
+        <label htmlFor={name} className="label">
 					<span className="label-text text-white font-sarpanch font-extrabold">
 						{label}
 					</span>
-				</label>
-			)}
-			<div className="relative">
-				<input
-					id={name}
-					className={cn(
-						'input input-bordered w-full border-primary focus:border-primary bg-neutral',
-						errors[name] && 'input-error',
-						className
-					)}
-					{...inputProps}
-					{...register(name, {
-						valueAsNumber: inputProps.type === 'number'
-					})}
-				/>
-			</div>
+        </label>
+      )}
+      <input
+        id={name}
+        className={cn(
+          "input input-bordered w-full border-primary focus:border-primary bg-neutral",
+          errors[name] && "input-error",
+          className
+        )}
+        {...inputProps}
+        {...register(name, {
+          valueAsNumber: inputProps.type === "number"
+        })}
+      />
 
-			{errors[name] && (
-				<span className="mt-2 text-sm text-error">
+      {errors[name] && (
+        <span className="mt-2 text-sm text-error">
 					{errors[name]?.message?.toString()}
 				</span>
-			)}
-		</div>
-	);
+      )}
+    </div>
+  );
 };
