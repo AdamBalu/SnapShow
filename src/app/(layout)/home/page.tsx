bb398fb7e7db3a3a0cb4a838a44747d23d2f002f
AdamBalu <a.baluseskul@gmail.com>
2024-05-08 02:32:17 +0200

    feat: sign in screen moved to homepage

--- src/app/(layout)/home/page.tsx
+++ src/app/(layout)/home/page.tsx
@@ -2,14 +2,23 @@ import React from 'react';
 
 import { db } from '@/db';
 import { genres } from '@/db/schema/genre';
+import { Banner } from '@/components/banner';
+import { ExploreEventsButton } from '@/components/home/explore-events-button';
 
 const Page = async () => {
 	const genreList = await db.select().from(genres);
 	return (
 		<div>
-			{genreList.map(genre => (
-				<div key={genre.id}>{genre.name}</div>
-			))}
+			<div className="flex flex-col lg:mx-24 md:justify-around gap-10 md:flex-row h-screen mt-60">
+				<Banner />
+				<ExploreEventsButton />
+			</div>
+
+			<div id="main-page-start">
+				{genreList.map(genre => (
+					<div key={genre.id}>{genre.name}</div>
+				))}
+			</div>
 		</div>
 	);
 };
