47d6d11d5e731dd50cca74a8f466ece9594b69ae
AdamBalu <a.baluseskul@gmail.com>
2024-05-09 13:17:51 +0200

    feat: event detail improvements

--- src/components/event/event-detail.tsx
+++ src/components/event/event-detail.tsx
@@ -18,7 +18,7 @@ export const EventDetail = async ({ event }: EventDetailProps) => {
 	}
 	const eventGenres = await getEventGenres(event.id);
 	return (
-		<div className="w-full lg:w-[90%] relative xl:w-[70%] mt-16 font-extrabold bg-zinc-900 bg-opacity-70 rounded-2xl text-white flex flex-col gap-10">
+		<div className="w-full lg:w-[90%] relative xl:w-[70%] mt-12 font-extrabold bg-zinc-900 bg-opacity-70 rounded-2xl text-white flex flex-col gap-5">
 			<div>
 				<Image
 					className="rounded-tl-2xl rounded-tr-2xl"
