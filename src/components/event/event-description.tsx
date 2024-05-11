230c2ab1c567d57f6d15fd2c5417bdad094a3780
AdamBalu <a.baluseskul@gmail.com>
2024-05-11 10:11:33 +0200

    fix: event detail

--- src/components/event/event-description.tsx
+++ src/components/event/event-description.tsx
@@ -7,7 +7,6 @@ import {
 	getUserEventStatus,
 	getUsersActionOnEventCount
 } from '@/server-actions/events';
-import type { UserEventStatus } from '@/types/event-data';
 import VenueMap from '@/components/event/venue-map';
 
 import { EventDescriptionAttribute } from './event-description-attribute';
@@ -48,22 +47,19 @@ export const EventDescription = async ({
 
 	return (
 		<div className="flex flex-col-reverse lg:flex-row">
-			<div className="lg:w-2/5">
+			<div className="lg:w-3/5">
 				<VenueMap lat={Number(event.venueLat)} lng={Number(event.venueLng)} />
 			</div>
-			<div>
+			<div className="w-full">
 				<div className="flex w-full px-6 justify-between items-center gap-4">
 					<h1 className="text-xl sm:text-3xl md:text-2xl font-sarpanch font-extrabold">
 						{event.eventName}
 					</h1>
-					<div className="flex flex-col justify-end items-end mt-6">
+					<div className="flex flex-col justify-end items-end my-6">
 						<StatusButton
 							eventId={event.eventId}
 							initialStatus={userEventStatus}
 						/>
-						<span className="text-xs text-gray-600 text-end mt-3.5 mr-3">
-							you are {getStatusStr(userEventStatus)}
-						</span>
 					</div>
 				</div>
 				<div className="flex justify-between p-4">
@@ -79,32 +75,29 @@ export const EventDescription = async ({
 					</div>
 					<div>
 						<EventDescriptionAttribute
-							description={`${usersGoingCount} going`}
+							description={`${usersGoingCount}\u00A0going`}
 							icon={
 								<CircleCheck
 									size={24}
-									className="text-primary"
+									className="text-primary w-3 h-3 sm:w-6 sm:h-6"
 									strokeWidth={2}
 								/>
 							}
 						/>
 
 						<EventDescriptionAttribute
-							description={`${usersInterestedCount} interested`}
-							icon={<Star size={24} className="text-primary" strokeWidth={2} />}
+							description={`${usersInterestedCount}\u00A0interested`}
+							icon={
+								<Star
+									size={24}
+									className="text-primary w-3 h-3 sm:w-6 sm:h-6"
+									strokeWidth={2}
+								/>
+							}
 						/>
 					</div>
 				</div>
 			</div>
 		</div>
 	);
-};
-
-const getStatusStr = (s: UserEventStatus): string => {
-	switch (s) {
-		case 'not-interested':
-			return 'not interested';
-		default:
-			return s.toString();
-	}
 };
