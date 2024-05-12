4ea6800a7eaa5d00fe42d436f8adc13c79d7ab25
Tadeas <tedkozub@github.com>
2024-05-12 15:28:23 +0200

    fix: fix new post when user is not attending any events

--- src/components/post/new-post-form/new-post.tsx
+++ src/components/post/new-post-form/new-post.tsx
@@ -1,18 +1,19 @@
 'use client';
 
-import React, { useState } from 'react';
-import { z } from 'zod';
-import { FormProvider, useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
+import { useState } from 'react';
+import { FormProvider, useForm } from 'react-hook-form';
 import { toast } from 'sonner';
+import { z } from 'zod';
 
-import { Button } from '@/components/ui/button';
-import Modal from '@/components/ui/modal';
-import { FormTextArea } from '@/components/ui/form/form-textarea';
-import { cn } from '@/lib/cn';
-import { Avatar } from '@/components/user/avatar';
-import { FormSelect } from '@/components/ui/form/form-select';
 import { PostImagesUpload } from '@/components/post/new-post-form/post-images-upload';
+import { Button } from '@/components/ui/button';
+import { FormSelect } from '@/components/ui/form/form-select';
+import { FormTextArea } from '@/components/ui/form/form-textarea';
+import InfoBox from '@/components/ui/info-box';
+import Modal from '@/components/ui/modal';
+import { Avatar } from '@/components/user/avatar';
+import { cn } from '@/lib/cn';
 import { createPost } from '@/server-actions/posts';
 
 const newPostSchema = z.object({
@@ -59,43 +60,56 @@ export const NewPost = ({ userId, profilePicture, events }: NewPostProps) => {
 				className="max-w-4xl h-[85vh]"
 				close={() => setModalOpen(false)}
 			>
-				<FormProvider {...form}>
-					<form
-						onSubmit={form.handleSubmit(onSubmit)}
-						className="flex flex-col gap-2 w-full items-center "
-					>
-						<div className="flex w-full gap-5 items-center">
-							<Avatar
-								className="p-1 xl:p-1.5 size-16 xl:size-24 flex-shrink-0"
-								profilePicture={profilePicture}
-							/>
-							<FormTextArea className="w-auto" label="Comment" name="comment" />
-						</div>
-
-						<FormSelect
-							label="Event"
-							name="eventId"
-							options={events.map(event => ({
-								label: event.name,
-								value: event.id
-							}))}
-						/>
-
-						<PostImagesUpload name="images" />
-
-						<Button
-							disabled={form.formState.isSubmitting}
-							className={cn(
-								'btn btn-primary flex justify-center mt-4 w-full md:w-64'
-							)}
+				{!events || events.length === 0 ? (
+					<div className="mt-8">
+						<InfoBox>
+							Please go to event page and select events you are going to attend
+							to be able to add posts.
+						</InfoBox>
+					</div>
+				) : (
+					<FormProvider {...form}>
+						<form
+							onSubmit={form.handleSubmit(onSubmit)}
+							className="flex flex-col gap-2 w-full items-center "
 						>
-							Snap it!
-							{form.formState.isSubmitting && (
-								<span className="loading loading-spinner" />
-							)}
-						</Button>
-					</form>
-				</FormProvider>
+							<div className="flex w-full gap-5 items-center">
+								<Avatar
+									className="p-1 xl:p-1.5 size-16 xl:size-24 flex-shrink-0"
+									profilePicture={profilePicture}
+								/>
+								<FormTextArea
+									className="w-auto"
+									label="Comment"
+									name="comment"
+								/>
+							</div>
+
+							<FormSelect
+								label="Event"
+								name="eventId"
+								options={events.map(event => ({
+									label: event.name,
+									value: event.id
+								}))}
+							/>
+
+							<PostImagesUpload name="images" />
+
+							<Button
+								disabled={form.formState.isSubmitting || !events}
+								className={cn(
+									'btn btn-primary flex justify-center mt-4 w-full md:w-64'
+								)}
+							>
+								Snap it!
+								{form.formState.isSubmitting && (
+									<span className="loading loading-spinner" />
+								)}
+							</Button>
+						</form>
+					</FormProvider>
+				)}
 			</Modal>
 		</>
 	);
