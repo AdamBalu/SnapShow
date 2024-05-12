c979df269e9594b87752dff682b24693ba11c1ca
Vladimír Bouček <492927@mail.muni.cz>
2024-05-12 18:08:43 +0200

    fix: remove images when post uploaded

--- src/components/post/new-post-form/post-images-upload.tsx
+++ src/components/post/new-post-form/post-images-upload.tsx
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { toast } from 'sonner';
 import { Controller, useFormContext } from 'react-hook-form';
 import { X } from 'lucide-react';
 import Image from 'next/image';
 
 import { UploadButton } from '@/components/uploadthing/upload-button';
+import { useDeletePhotoMutation } from '@/hooks/delete-photo';
 
 type PostImagesUploadProps = {
 	name: string;
@@ -12,22 +13,24 @@ type PostImagesUploadProps = {
 
 export const PostImagesUpload = ({ name }: PostImagesUploadProps) => {
 	const [images, setImages] = useState<string[]>([]);
-	const { register, control, setValue } = useFormContext();
+	const {
+		register,
+		control,
+		setValue,
+		formState: { errors, isSubmitSuccessful }
+	} = useFormContext();
+	const { mutate } = useDeletePhotoMutation();
 
 	const onImageDelete = async (imageToDelete: string) => {
 		setImages([...images.filter(img => img !== imageToDelete)]);
-		await fetch('api/uploadthing', {
-			method: 'DELETE',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify({
-				url: imageToDelete
-			})
-		});
-
-		setValue(name, [...images.filter(img => img !== imageToDelete)]);
+		mutate(imageToDelete);
 	};
+
+	useEffect(() => {
+		if (isSubmitSuccessful) {
+			setImages([]);
+		}
+	}, [isSubmitSuccessful]);
 
 	return (
 		<div className="flex flex-col w-full h-full justify-between gap-4 flex-grow">
@@ -61,27 +64,33 @@ export const PostImagesUpload = ({ name }: PostImagesUploadProps) => {
 					</div>
 				)}
 			/>
-			<ul className="mt-2 flex flex-row h-80 overflow-scroll gap-6 flex-wrap justify-around md:justify-start p-5 rounded-lg">
-				{images.map(image => (
-					<li key={image} className="flex flex-col items-end">
-						<button
-							onClick={() => onImageDelete(image)}
-							className="-mb-8 mr-2 z-50 btn btn-circle btn-xs"
-						>
-							<X className="size-4" />
-						</button>
-						<Image
-							className=" w-full h-auto sm:h-32 sm:w-auto lg:h-40 rounded-lg"
-							src={image}
-							width="0"
-							height="0"
-							sizes="100vw"
-							objectFit="contain"
-							alt="uploaded image"
-						/>
-					</li>
-				))}
-			</ul>
+			{errors[name] && images.length === 0 ? (
+				<div className="mt-2 text-sm text-error h-80">
+					Post must have at least one image.
+				</div>
+			) : (
+				<ul className="mt-2 flex flex-row h-80 overflow-scroll gap-6 flex-wrap justify-around md:justify-start p-5 rounded-lg">
+					{images.map(image => (
+						<li key={image} className="flex flex-col items-end">
+							<button
+								onClick={() => onImageDelete(image)}
+								className="-mb-8 mr-2 z-50 btn btn-circle btn-xs"
+							>
+								<X className="size-4" />
+							</button>
+							<Image
+								className=" w-full h-auto sm:h-32 sm:w-auto lg:h-40 rounded-lg"
+								src={image}
+								width="0"
+								height="0"
+								sizes="100vw"
+								objectFit="contain"
+								alt="uploaded image"
+							/>
+						</li>
+					))}
+				</ul>
+			)}
 		</div>
 	);
 };
