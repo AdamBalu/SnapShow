f2e5c657903927f4744f47a8f49764650a6d557b
Igor Hanus <igorhanus@thinkpad.local>
2024-05-14 11:24:19 +0200

    feat: loading page

---
+++ src/app/(layout)/loading.tsx
@@ -0,0 +1,9 @@
+import { LogoLoader } from '@/components/logo-loader';
+
+const Loading = () => (
+	<div className="w-full flex justify-center">
+		<LogoLoader />
+	</div>
+);
+
+export default Loading;
